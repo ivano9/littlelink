b2867c0a0027937f4dc4efde4b04f410bed137b9
Ivan Giovanazzi <ivanog@pm.me>
2020-11-20 02:25:37 -0300

    se agregó google analytics

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Google Analytics
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q236PJE5HT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Q236PJE5HT');
+</script>
